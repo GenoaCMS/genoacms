a574b01653497dec7c22c18783b55d33d5a0ee9e
Filip Holčík <filip.holcik.official@gmail.com>
2023-10-28 22:00:14 +0200

    feat: created basic types for storage

---
+++ packages/cloudAbstraction/src/services/storage/index.d.ts
@@ -0,0 +1,29 @@
+interface StorageObject {
+  name: string
+  size: number
+  lastModified: Date
+}
+interface ObjectData {
+  data: ReadableStream
+}
+
+interface DirectoryListingParams {
+  prefix?: string
+  startAfter?: string
+  limit?: number
+}
+
+type getObject = (name: string) => Promise<ObjectData>
+type uploadObject = (name: string, stream: ReadableStream) => Promise<void>
+type deleteObject = (name: string) => Promise<void>
+type listDirectory = (params: DirectoryListingParams) => Promise<StorageObject[]>
+type createDirectory = (name: string) => Promise<void>
+
+export type {
+  StorageObject,
+  getObject,
+  uploadObject,
+  deleteObject,
+  listDirectory,
+  createDirectory
+}
