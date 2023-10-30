8b7618c270ce7be96ef76661a19ed7c017165e22
Filip Holčík <filip.holcik.official@gmail.com>
2023-10-30 23:02:53 +0100

    feat: adapter config import script

--- packages/cloudAbstraction/src/index.d.ts
+++
@@ -1,9 +0,0 @@
-import type * as auth from './services/auth'
-import type * as database from './services/database'
-import type * as storage from './services/storage'
-
-export type {
-  auth,
-  database,
-  storage
-}
