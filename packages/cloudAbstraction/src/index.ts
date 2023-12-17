474cc0cbf8a08b76cc0a364b5042b6cc7d5f955d
Filip Holčík <filip.holcik.official@gmail.com>
2023-12-17 16:30:51 +0100

    0.2.0

--- packages/cloudAbstraction/src/index.ts
+++ packages/cloudAbstraction/src/index.ts
@@ -1,12 +1,14 @@
 import type * as auth from './services/auth/index.d.ts'
 import type * as database from './services/database/index.d.ts'
 import type * as storage from './services/storage/index.d.ts'
+import type genoaConfig from './genoa.config.d.ts'
 import config from './config.js'
 
 export type {
   auth,
   database,
-  storage
+  storage,
+  genoaConfig
 }
 export {
   config
