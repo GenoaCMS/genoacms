b78744ea8f5b0573fce031e8268474e1b107c3f7
Filip Holčík <filip.holcik.official@gmail.com>
2023-12-17 16:25:20 +0100

    feat: added basic type declarations for database

--- packages/cloudAbstraction/src/services/database/index.d.ts
+++ packages/cloudAbstraction/src/services/database/index.d.ts
@@ -1,10 +1,33 @@
 import type { JSONSchemaType } from 'ajv'
 
-interface Collection {
+interface CollectionReference {
   name: string
   schema: JSONSchemaType<any>
 }
+interface DocumentReference<C extends CollectionReference> {
+  collection: C
+  id: string
+}
+type Document<C extends CollectionReference> = Record<keyof C, C[key]>
+type Collection<C extends CollectionReference> = Array<Document<C>>
+
+type createDocument = <C extends CollectionReference>(reference: C, document: Document<C>) => Promise<DocumentReference<C>>
+
+type getCollection = <C extends CollectionReference>(reference: C) => Promise<Collection<C>>
+type getDocument = <C extends CollectionReference, D extends DocumentReference<C>>(reference: D) => Promise<Document<C>>
+
+type updateDocument = <C extends CollectionReference>(reference: C, document: Document<C>) => Promise<DocumentReference<C>>
+
+type deleteDocument = <C extends CollectionReference>(reference: C, document: Document<C>) => Promise<void>
 
 export type {
-  Collection
+  CollectionReference,
+  DocumentReference,
+  Collection,
+  Document,
+  createDocument,
+  getCollection,
+  getDocument,
+  updateDocument,
+  deleteDocument
 }
