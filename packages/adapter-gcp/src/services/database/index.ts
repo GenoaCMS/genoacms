5591f9d77841ae704584087ef60e173f86a57ea3
Filip Holčík <filip.holcik.official@gmail.com>
2023-12-18 15:42:20 +0100

    feat: prepared firestore adapter and implemented getDocument
    Signed-off-by: Filip Holčík <filip.holcik.official@gmail.com>

---
+++ packages/adapter-gcp/src/services/database/index.ts
@@ -0,0 +1,37 @@
+import { Firestore } from '@google-cloud/firestore'
+import config from '../../config.js'
+import type { database as databaseT } from '@genoacms/cloudabstraction'
+
+const firestore = new Firestore({
+  credentials: config.database.credentials,
+  databaseId: config.database.databaseId,
+  projectId: config.database.projectId
+})
+
+const getDocument: databaseT.getDocument = async ({ collection, id }) => {
+  const document = await firestore.collection(collection.name).doc(id).get()
+  return document.data() as any
+}
+
+// Test section:
+
+// const doc = await getDocument({
+//   collection: {
+//     name: 'test',
+//     schema: {
+//       type: 'object',
+//       properties: {
+//         id: {
+//           type: 'string'
+//         }
+//       }
+//     }
+//   },
+//   id: 'zEhn42fLbfWkXwvUDzFt'
+// })
+//
+// console.log(doc)
+
+export {
+  getDocument
+}
