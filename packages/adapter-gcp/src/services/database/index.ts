29e4c64c5b28af2c97c59618e625822882ca6932
Filip Holčík <filip.holcik.official@gmail.com>
2023-12-18 18:48:28 +0100

    feat: implemented createDocument, getCollection, updateDocument, deleteDocument functions and their tests
    Signed-off-by: Filip Holčík <filip.holcik.official@gmail.com>

--- packages/adapter-gcp/src/services/database/index.ts
+++ packages/adapter-gcp/src/services/database/index.ts
@@ -8,11 +8,46 @@ const firestore = new Firestore({
   projectId: config.database.projectId
 })
 
+const createDocument: databaseT.createDocument = async (reference, data ) => {
+  const document = await firestore.collection(reference.name).add(data)
+  return {
+    collection: reference,
+    id: document.id
+  }
+}
+
+const getCollection: databaseT.getCollection = async ({ name, schema }) => {
+  const collection = await firestore.collection(name).get()
+  const documents: databaseT.Document<typeof schema>[] = []
+  collection.forEach(document => {
+    let documentData: databaseT.Document<typeof schema> = {}
+    Object.keys(schema.properties).forEach(key => {
+      documentData[key] = document.get(key)
+    })
+
+    documents.push(document.data())
+  })
+  return documents
+}
+
 const getDocument: databaseT.getDocument = async ({ collection, id }) => {
   const document = await firestore.collection(collection.name).doc(id).get()
   return document.data() as any
 }
 
+const updateDocument: databaseT.updateDocument = async (reference, document) => {
+  await firestore.collection(reference.collection.name).doc(reference.id).update(document)
+  return reference
+}
+
+const deleteDocument: databaseT.deleteDocument = async (reference) => {
+  await firestore.collection(reference.collection.name).doc(reference.id).delete()
+}
+
 export {
-  getDocument
+  createDocument,
+  getDocument,
+  getCollection,
+  updateDocument,
+  deleteDocument
 }
--- packages/adapter-gcp/test/database.test.js
+++ packages/adapter-gcp/test/database.test.js
@@ -1,26 +1,72 @@
-import { getDocument } from '../dist/services/database/index.js'
-import { test, expect } from 'vitest'
+import {
+  createDocument,
+  deleteDocument,
+  getCollection,
+  getDocument,
+  updateDocument
+} from '../dist/services/database/index.js'
+import { suite, test, it, expect } from 'vitest'
 
-test('getDocument', async () => {
-  const doc = await getDocument({
-    collection: {
-      name: 'test',
-      schema: {
-        type: 'object',
-        properties: {
-          name: {
-            type: 'string'
-          },
-          isA: {
-            type: 'boolean'
-          }
-        }
+const testCollection = {
+  name: 'test',
+  schema: {
+    type: 'object',
+    properties: {
+      name: {
+        type: 'string'
+      },
+      isA: {
+        type: 'boolean'
       }
-    },
-    id: 'zEhn42fLbfWkXwvUDzFt'
+    }
+  }
+}
+
+suite('complex test', async () => {
+  let id
+  let documentData = { name: 'createDocument', isA: true }
+  it('is creating a document', async () => {
+    const doc = await createDocument(testCollection, documentData)
+    expect(doc).toMatchObject({
+      collection: testCollection
+    })
+    id = doc.id
   })
-  expect(doc).toEqual({
-    name: 'sss',
-    isA: true
+  it('is getting a document', async () => {
+    const doc = await getDocument({
+      collection: testCollection,
+      id
+    })
+    expect(doc).toEqual(documentData)
+  })
+  documentData = { name: 'updateDocument', isA: false }
+  it('is updating a document', async () => {
+    let documentReference = {
+      collection: testCollection,
+      id
+    }
+    const doc = await updateDocument(documentReference, documentData)
+    expect(doc).toBe(documentReference)
+  })
+  it('is getting a document again', async () => {
+    const doc = await getDocument({
+      collection: testCollection,
+      id
+    })
+    expect(doc).toEqual(documentData)
+  })
+  it('is deleting a document', async () => {
+    let documentReference = {
+      collection: testCollection,
+      id
+    }
+    const doc = await deleteDocument(documentReference, documentData)
+    expect(doc).toBeUndefined()
+  })
+  it('lists collection', async () => {
+    const collection = await getCollection(testCollection)
+    expect(collection).toBeInstanceOf(Array)
+    expect(collection.length).toBeGreaterThan(1)
   })
 })
+
