bb93aed7b153ae9fbca98c1f1f9637440aaf16cc
Filip Holčík <filip.holcik.official@gmail.com>
2023-12-18 17:00:24 +0100

    fix: database typing for getting, updating, deleting document

--- packages/cloudAbstraction/src/services/database/index.ts
+++ packages/cloudAbstraction/src/services/database/index.ts
@@ -14,11 +14,11 @@ type Collection<C extends CollectionReference> = Array<Document<C>>
 type createDocument = <C extends CollectionReference>(reference: C, document: Document<C>) => Promise<DocumentReference<C>>
 
 type getCollection = <C extends CollectionReference>(reference: C) => Promise<Collection<C>>
-type getDocument = <C extends CollectionReference, D extends DocumentReference<C>>(reference: D) => Promise<Document<C>>
+type getDocument = <C extends CollectionReference, D extends DocumentReference<C>>(reference: D) => Promise<Document<C> | undefined>
 
-type updateDocument = <C extends CollectionReference>(reference: C, document: Document<C>) => Promise<DocumentReference<C>>
+type updateDocument = <C extends CollectionReference, D extends DocumentReference<C>>(reference: D, document: Document<C>) => Promise<DocumentReference<C>>
 
-type deleteDocument = <C extends CollectionReference>(reference: C, document: Document<C>) => Promise<void>
+type deleteDocument = <C extends CollectionReference, D extends DocumentReference<C>>(reference: D) => Promise<void>
 
 export type {
   CollectionReference,
