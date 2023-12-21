b813a500f4f8fb67e7f89adb003f843253bea744
Filip Holčík <filip.holcik.official@gmail.com>
2023-12-22 00:16:33 +0100

    feat: implemented snapshots for database
    Signed-off-by: Filip Holčík <filip.holcik.official@gmail.com>

--- packages/adapter-gcp/src/services/database/index.ts
+++ packages/adapter-gcp/src/services/database/index.ts
@@ -10,34 +10,57 @@ const firestore = new Firestore({
 
 const createDocument: databaseT.createDocument = async (reference, data) => {
   const document = await firestore.collection(reference.name).add(data)
-  return {
+  const documentReference: databaseT.DocumentReference<typeof reference> = {
     collection: reference,
     id: document.id
   }
+  return {
+    reference: documentReference,
+    data
+  } satisfies databaseT.DocumentSnapshot<typeof reference>
 }
 
-const getCollection: databaseT.getCollection = async ({ name, schema }) => {
-  const collection = await firestore.collection(name).get()
-  const documents: Array<databaseT.Document<typeof schema>> = []
+const getCollection: databaseT.getCollection = async (reference) => {
+  const collection = await firestore.collection(reference.name).get()
+  const documents: databaseT.CollectionSnapshot<typeof reference> = []
+
   collection.forEach(document => {
-    const documentData: databaseT.Document<typeof schema> = {}
-    Object.keys(schema.properties).forEach(key => {
+    const documentData: databaseT.Document<typeof reference.schema> = {}
+    Object.keys(reference.schema.properties).forEach(key => {
       documentData[key] = document.get(key)
     })
 
-    documents.push(document.data())
+    documents.push({
+      reference: {
+        collection: reference,
+        id: document.id
+      },
+      data: document.data() as databaseT.Document<typeof reference.schema>
+    })
   })
   return documents
 }
 
 const getDocument: databaseT.getDocument = async ({ collection, id }) => {
   const document = await firestore.collection(collection.name).doc(id).get()
-  return document.data() as any
+  if (!document.exists) return undefined
+  const documentReference: databaseT.DocumentReference<typeof collection> = {
+    collection,
+    id
+  }
+  const documentSnapshot: databaseT.DocumentSnapshot<typeof collection> = {
+    reference: documentReference,
+    data: document.data() as databaseT.Document<typeof collection>
+  }
+  return documentSnapshot
 }
 
 const updateDocument: databaseT.updateDocument = async (reference, document) => {
   await firestore.collection(reference.collection.name).doc(reference.id).update(document)
-  return reference
+  return {
+    reference,
+    data: document
+  } satisfies databaseT.UpdateSnapshot<typeof reference.collection>
 }
 
 const deleteDocument: databaseT.deleteDocument = async (reference) => {
