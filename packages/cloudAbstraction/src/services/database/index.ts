76a5cf46a88b1966ba1598d4621f6d477f82d354
Filip Holčík <filip.holcik.official@gmail.com>
2023-12-21 17:25:38 +0100

    feat: added primary key to collection reference and added optional query parameters to getCollection

--- packages/cloudAbstraction/src/services/database/index.ts
+++ packages/cloudAbstraction/src/services/database/index.ts
@@ -2,18 +2,31 @@ import type { JSONSchemaType } from 'ajv'
 
 interface CollectionReference {
   name: string
+  primaryKey: string
   schema: JSONSchemaType<any>
 }
 interface DocumentReference<C extends CollectionReference> {
   collection: C
   id: string
 }
+interface QueryParams {
+  startAfter?: {
+    field: string
+    value: string
+  }
+  limit?: number
+  conditions?: [{
+    field: string
+    operator: string
+    value: string
+  }]
+}
 type Document<C extends CollectionReference> = Record<keyof C, C[keyof C]>
 type Collection<C extends CollectionReference> = Array<Document<C>>
 
 type createDocument = <C extends CollectionReference>(reference: C, document: Document<C>) => Promise<DocumentReference<C>>
 
-type getCollection = <C extends CollectionReference>(reference: C) => Promise<Collection<C>>
+type getCollection = <C extends CollectionReference>(reference: C, queryParams?: QueryParams) => Promise<Collection<C>>
 type getDocument = <C extends CollectionReference, D extends DocumentReference<C>>(reference: D) => Promise<Document<C> | undefined>
 
 type updateDocument = <C extends CollectionReference, D extends DocumentReference<C>>(reference: D, document: Document<C>) => Promise<DocumentReference<C>>
@@ -24,6 +37,7 @@ export type {
   CollectionReference,
   DocumentReference,
   Collection,
+  QueryParams,
   Document,
   createDocument,
   getCollection,
