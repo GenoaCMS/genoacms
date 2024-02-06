57b3dccefef73ddeea20d7f6f4a02b0d4c50ae42
Filip Holčík <filip.holcik.official@gmail.com>
2024-02-06 19:34:42 +0100

    feat: separated list of directories from files in listDirectory

--- packages/cloudAbstraction/src/services/storage/index.ts
+++ packages/cloudAbstraction/src/services/storage/index.ts
@@ -2,24 +2,33 @@ interface ObjectReference {
   bucket: string
   name: string
 }
+
 interface StorageObject {
   name: string
   size: number
   lastModified: Date
 }
+
 type objectPayload = string | Buffer | NodeJS.ReadableStream
+
 interface ObjectData {
   data: NodeJS.ReadableStream
 }
+
 interface DirectoryListingParams {
   startAfter?: string
   limit?: number
 }
 
+interface DirectoryContents {
+  files: StorageObject[]
+  directories: string[]
+}
+
 type getObject = (reference: ObjectReference) => Promise<ObjectData>
 type uploadObject = (reference: ObjectReference, data: objectPayload) => Promise<void>
 type deleteObject = (reference: ObjectReference) => Promise<void>
-type listDirectory = (reference: ObjectReference, params?: DirectoryListingParams) => Promise<StorageObject[]>
+type listDirectory = (reference: ObjectReference, params?: DirectoryListingParams) => Promise<DirectoryContents>
 type createDirectory = (reference: ObjectReference) => Promise<void>
 
 export type {
