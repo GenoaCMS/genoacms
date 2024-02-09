815f52f90b6d059eaad753b2db50df6ab9c5f1b2
Filip Holčík <filip.holcik.official@gmail.com>
2024-02-09 13:05:44 +0100

    feat(components): typed pre-built component schema

--- packages/core/src/lib/script/components.server.ts
+++ packages/core/src/lib/script/components.server.ts
@@ -41,7 +41,77 @@ const getComponent = async (name: string) => {
   return JSON.parse(text)
 }
 
+interface Attribute {
+  name: string,
+  description: string,
+  isRequired: boolean
+}
+
+interface BooleanAttribute extends Attribute {
+  type: 'boolean',
+  defaultValue: boolean
+}
+
+interface NumberAttribute extends Attribute {
+  type: 'number',
+  min: number,
+  max: number,
+  defaultValue: number,
+  step: number,
+  decimalPlaces: number
+}
+
+interface StringAttribute extends Attribute {
+  type: 'string',
+  regex: string,
+  maxLength: number,
+  defaultValue: string
+}
+
+interface TextAttribute extends Attribute {
+  type: 'text',
+  maxLength: number,
+  defaultValue: string
+}
+
+interface MarkdownAttribute extends Attribute {
+  type: 'markdown',
+  defaultValue: string
+}
+
+interface RichTextAttribute extends Attribute {
+  type: 'richText',
+  defaultValue: string
+}
+
+interface LinkAttribute extends Attribute {
+  type: 'link',
+}
+
+interface StorageResourceAttribute extends Attribute {
+  type: 'storageResource',
+}
+
+interface ComponentsAttribute extends Attribute {
+  type: 'component',
+  component: string,
+  maxComponents: number,
+  allowedComponents: Array<string>
+}
+
+type attributeValue = BooleanAttribute | NumberAttribute | StringAttribute | TextAttribute | MarkdownAttribute
+  | RichTextAttribute | LinkAttribute | StorageResourceAttribute | ComponentsAttribute
+
+interface Component {
+  version: string,
+  attributes: Record<string, attributeValue>
+}
+
 export {
   listOrCreatePreBuiltComponentList,
   getComponent
+}
+export type {
+  Component,
+  attributeValue
 }
