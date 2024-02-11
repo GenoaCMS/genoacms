c7e9a66d5b81e00a72f9e8ce63520783992f1cda
Filip Holčík <filip.holcik.official@gmail.com>
2024-02-11 16:02:30 +0100

    feat: made CardLink.svelte usable as link or button

--- packages/core/src/lib/components/CardLink.svelte
+++ packages/core/src/lib/components/CardLink.svelte
@@ -1,16 +1,18 @@
 <script lang="ts">
+    import CardLinkContents from '$lib/components/CardLinkContents.svelte'
+
     export let icon: string
     export let text: string
-    export let href: string
+    export let href: string = ''
 </script>
 
-<a {href} on:contextmenu|preventDefault|stopPropagation {...$$restProps}>
-    <div class="border border-dark border-opacity-30 aspect-square flex flex-col">
-        <div class="flex-grow flex justify-center items-center">
-            <i class="bi bi-{icon} text-6xl"/>
-        </div>
-        <div class="text-center border-t border-t-dark text-xl border-opacity-30 p-1 text-nowrap whitespace-nowrap truncate">
-            {text}
-        </div>
-    </div>
-</a>
+{#if href}
+    <a {href} on:contextmenu|preventDefault|stopPropagation {...$$restProps}>
+        <CardLinkContents {icon} {text} />
+    </a>
+{:else}
+    <button type="button" on:click on:contextmenu|preventDefault|stopPropagation {...$$restProps}>
+        <CardLinkContents {icon} {text} />
+    </button>
+{/if}
+
---
+++ packages/core/src/lib/components/CardLinkContents.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  export let icon: string
+  export let text: string
+</script>
+
+<div class="border border-dark border-opacity-30 aspect-square flex flex-col">
+    <div class="flex-grow flex justify-center items-center">
+        <i class="bi bi-{icon} text-6xl"/>
+    </div>
+    <div class="text-center border-t border-t-dark text-xl border-opacity-30 p-1 text-nowrap whitespace-nowrap truncate">
+        {text}
+    </div>
+</div>
