2dbe95fed1d692321c07ea922f667855400b8166
Filip Holčík <filip.holcik.official@gmail.com>
2024-03-03 22:52:51 +0100

    feat: implemented loginWithEmailAndPassword

--- packages/authentication-adapter-array/src/index.js
+++ packages/authentication-adapter-array/src/index.js
@@ -1,1 +1,19 @@
 import { config } from '@genoacms/cloudabstraction'
+
+/**
+ * @type {import('@genoacms/cloudabstraction').authentication.loginWithEmailAndPassword}
+ */
+async function loginWithEmailAndPassword (email, password) {
+    /**
+     * @type {import('./config'.credentialsArray).credentialsArray | undefined}
+     */
+    const credentialsArray = config?.authentication?.credentials
+    if (!credentialsArray) throw new Error('missing-credentials')
+    const credentials = credentialsArray.find(c => c.email === email)
+    if (!credentials) throw new Error('invalid-credentials')
+    return credentials.password === password
+}
+
+export {
+    loginWithEmailAndPassword
+}
