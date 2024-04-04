4c842ab6f53ef15538cc5516f66da226ccfe01b5
Filip Holčík <filip.holcik.official@gmail.com>
2024-04-04 19:10:46 +0200

    feat(config): added switchable config for aws

---
+++ packages/core/genoa.config/aws/index.js
@@ -0,0 +1,45 @@
+import authCredentials from './authCredentials.js'
+import 'dotenv/config'
+
+const credentials = {
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+}
+
+/**
+ * @type {import('@genoacms/cloudabstraction').genoaConfig}
+ */
+const config = {
+  authentication: {
+    adapter: import('@genoacms/authentication-adapter-array'),
+    credentials: authCredentials,
+    cookieName: '__session',
+    JWTSecret: 'genoacms123' // In real world deployment, pass from environment variable
+  },
+  authorization: {
+    adapter: import('@genoacms/adapter-aws/authorization'),
+    region: 'eu-west-2',
+    credentials
+  },
+  database: {
+    adapter: import('@genoacms/adapter-aws/database'),
+    region: 'eu-west-2',
+    credentials
+  },
+  deployment: {
+    adapter: import('@genoacms/adapter-aws/deployment'),
+    region: 'eu-west-2',
+    credentials
+  },
+  storage: {
+    adapter: import('@genoacms/adapter-aws/storage'),
+    region: 'eu-central-1',
+    defaultBucket: 'genoacms.test',
+    buckets: [
+      'genoacms.test'
+    ],
+    credentials
+  }
+}
+
+export default config
---
+++ packages/core/genoa.config/gcp/index.js
@@ -0,0 +1,44 @@
+import credentials from './serviceAccount.json' assert { type: 'json' }
+import authCredentials from './authCredentials.js'
+
+/**
+ * @type {import('@genoacms/cloudabstraction').genoaConfig}
+ */
+const config = {
+  authentication: {
+    adapter: import('@genoacms/authentication-adapter-array'),
+    credentials: authCredentials,
+    cookieName: '__session',
+    JWTSecret: 'genoacms123' // In real world deployment, pass from environment variable
+  },
+  authorization: {
+    adapter: import('@genoacms/adapter-gcp/authorization'),
+    projectId: 'genoacms',
+    credentials
+  },
+  database: {
+    adapter: import('@genoacms/adapter-gcp/database'),
+    region: 'eu-west3',
+    databaseId: '(default)',
+    projectId: 'genoacms',
+    credentials
+  },
+  deployment: {
+    adapter: import('@genoacms/adapter-gcp/deployment'),
+    projectId: 'genoacms',
+    region: 'europe-west3',
+    credentials
+  },
+  storage: {
+    adapter: import('@genoacms/adapter-gcp/storage'),
+    projectId: 'genoacms',
+    defaultBucket: 'genoacms.test',
+    buckets: [
+      'genoa-public',
+      'genoacms.test'
+    ],
+    credentials
+  }
+}
+
+export default config
