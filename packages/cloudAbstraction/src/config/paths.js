4ab716b9f259347c0d3c735c75c2130a3177a76a
Filip Holčík <filip.holcik.official@gmail.com>
2024-07-06 18:29:15 +0200

    fix(config): paths

--- packages/cloudAbstraction/src/config/paths.js
+++ packages/cloudAbstraction/src/config/paths.js
@@ -3,7 +3,7 @@ import { join } from 'path'
 const workDir = process.cwd()
 const configDirectory = join(workDir, 'genoa.config')
 const configPath = join(configDirectory, 'index.js')
-const configBundlePath = join(workDir, 'build', 'genoa.config', 'index.js')
+const configBundlePath = join(workDir, 'build', 'genoa.config')
 
 export {
   configPath,
