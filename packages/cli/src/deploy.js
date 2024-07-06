93c60a3e4bb8d3d6da6722c34a224b44e3c91355
Filip Holčík <filip.holcik.official@gmail.com>
2024-07-06 18:36:53 +0200

    feat(deploy): deploy progress spinners

--- packages/cli/src/deploy.js
+++ packages/cli/src/deploy.js
@@ -3,19 +3,38 @@ import { exec as execCb } from 'node:child_process'
 import { config } from '@genoacms/cloudabstraction'
 import { copyConfig } from './utils.js'
 import buildConfig from '@genoacms/cloudabstraction/configBuilder'
+import { spinner } from '@clack/prompts'
 
 const isProd = !process.argv.includes('--dev')
 const exec = promisify(execCb)
 
 async function deploy () {
   const { deployProcedure } = await config.deployment.adapter
+  const preparingPaths = spinner()
+  const buildingCMS = spinner()
+  const buildingConfig = spinner()
+  const deploying = spinner()
+
   if (isProd) {
+    preparingPaths.start('Preparing build paths')
+
     await copyConfig()
     process.chdir('node_modules/@genoacms/core')
+
+    preparingPaths.stop('Build paths prepared')
   }
+
+  buildingCMS.start('Building CMS code')
   await exec('npm run build')
+  buildingCMS.stop('CMS code built')
+
+  buildingConfig.start('Building config')
   await buildConfig()
-  deployProcedure()
+  buildingConfig.stop('Config built')
+
+  deploying.start('Deploying code')
+  await deployProcedure()
+  deploying.stop('Code deployed')
 }
 
 export default deploy
