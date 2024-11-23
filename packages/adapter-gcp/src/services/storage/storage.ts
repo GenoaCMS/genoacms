34bea91bd1d4ae39ee064c64bee8af7959a702e7
Filip Holčík <filip.holcik.official@gmail.com>
2024-11-23 19:10:20 +0100

    fix(storage): resolving bucket

--- packages/adapter-gcp/src/services/storage/storage.ts
+++ packages/adapter-gcp/src/services/storage/storage.ts
@@ -1,15 +1,21 @@
 import config from '../../config.js'
 import { type Bucket, Storage } from '@google-cloud/storage'
-import type { StorageProvider } from '@genoacms/cloudabstraction/storage'
+import type { StorageProvider, BucketInit } from '@genoacms/cloudabstraction/storage'
 
-const storageConfig = config.storage.providers.find((provider: StorageProvider) => provider.name === 'gcs')
+const PROVIDER_NAME = '@genoacms/adapter-gcp/storage'
+const storageConfig = config.storage.providers.find((provider: StorageProvider) => provider.name === PROVIDER_NAME)
 if (!storageConfig) throw new Error('storage-provider-not-found')
 const storage = new Storage({
   credentials: config.storage.credentials
 })
 
+const hasBucket = (name: string): boolean => {
+  const has = config.storage.buckets.find((bucket: BucketInit) => bucket.name === name && bucket.providerName === PROVIDER_NAME)
+  return !!has
+}
+
 const getBucket = (name: string): Bucket => {
-  if (!config.storage.buckets.includes(name)) throw new Error('bucket-unregistered')
+  if (!hasBucket(name)) throw new Error('bucket-unregistered')
   const bucket = storage.bucket(name)
   return bucket
 }
