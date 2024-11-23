d24cbd1359a14313059072232b6c72d17f928c53
Filip Holčík <filip.holcik.official@gmail.com>
2024-11-23 18:01:21 +0100

    feat: updated to provider format

--- packages/authentication-adapter-array/src/index.js
+++ packages/authentication-adapter-array/src/index.js
@@ -7,7 +7,9 @@ async function loginWithEmailAndPassword (email, password) {
     /**
      * @type {import('./config'.credentialsArray).credentialsArray | undefined}
      */
-    const credentialsArray = config?.authentication?.credentials
+    const providerConfig = config?.authentication?.providers.find(p => p.name === '@genoacms/authentication-adapter-array')
+    if (!providerConfig) throw new Error('missing-provider-config')
+    const credentialsArray = providerConfig.credentials
     if (!credentialsArray) throw new Error('missing-credentials')
     const credentials = credentialsArray.find(c => c.email === email)
     if (!credentials) throw new Error('invalid-credentials')
