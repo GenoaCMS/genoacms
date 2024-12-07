fe9473103c2782a4fcbce2927121ffdc1e224b4e
Filip Holčík <filip.holcik.official@gmail.com>
2024-12-08 00:07:48 +0100

    feat(collections): StorageResource

---
+++ packages/core/src/routes/(admin)/collections/[collectionName]/Editor/StorageResource/StorageResource.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+  import StorageObject from './StorageObject.svelte'
+  import EditSelection from './EditSelection.svelte'
+  import { onDestroy } from 'svelte'
+  import { ITC } from '$lib/script/utils'
+  import type { ObjectReference } from '@genoacms/cloudabstraction/storage'
+
+  export let name: string
+  export let resource: ObjectReference | undefined
+  const selectionId = crypto.randomUUID()
+  const itc = new ITC(selectionId)
+
+  function deleteResource () {
+    resource = undefined
+  }
+
+  itc.on('selectionInit', async () => {
+    const parameters = {
+      maxItems: 1
+    }
+    const selectionInitData = {
+      parameters,
+      defaultValue: resource ? [resource] : []
+    }
+    itc.send('selectionInitData', selectionInitData)
+    const selection = await itc.once('selectionDone') as Array<ObjectReference>
+    itc.send('selectionKill')
+    if (selection.length < 1) return
+    resource = selection[0]
+  })
+
+  onDestroy(() => {
+    itc.close()
+  })
+</script>
+
+<input type="hidden" {name} value={JSON.stringify(resource)}>
+<div class="flex flex-col">
+  {#if resource}
+    <StorageObject {resource} {deleteResource}/>
+  {:else}
+    <EditSelection {selectionId} hideDeleteButton/>
+  {/if}
+</div>
