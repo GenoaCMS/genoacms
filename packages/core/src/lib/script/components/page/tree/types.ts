d66b5ab13a7622714d3dac8a4a22e6eebda5c8af
Filip Holčík <filip.holcik.official@gmail.com>
2025-02-13 19:50:06 +0100

    chore(components): updated ReadablePageNode type

--- packages/core/src/lib/script/components/page/tree/types.ts
+++ packages/core/src/lib/script/components/page/tree/types.ts
@@ -1,7 +1,8 @@
 type ReadableAttributeValue = boolean | number | string | Array<ReadablePageNode>
 
-type ReadablePageNode = {
-  component: string,
+type ReadablePageNode<isComponentPrebuilt extends boolean> = {
+  componentName: isComponentPrebuilt extends true ? string : undefined,
+  componentCode: isComponentPrebuilt extends false ? string : undefined,
   data: Record<string, ReadableAttributeValue>
 }
 export type {
