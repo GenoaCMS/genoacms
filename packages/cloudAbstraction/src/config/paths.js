ac962d133395526ce9813fb37eb57179fa0d4696
Filip Holčík <filip.holcik.official@gmail.com>
2025-03-13 15:15:14 +0100

    fix(config): typo

--- packages/cloudAbstraction/src/config/paths.js
+++ packages/cloudAbstraction/src/config/paths.js
@@ -1,7 +1,7 @@
 import { join } from 'path'
 
 const workDir = process.cwd()
-const configWhileBuild = process.env.GENOA_BUILD === 'true' ? '' : join('..', '..', '..')
+const configWhileBuild = process.env.GENOA_BUILD === 'true' ? join('..', '..', '..') : ''
 const configDirectory = join(workDir, configWhileBuild, process.env.GENOA_CONFIG_PATH ?? 'genoa.config')
 const configPath = join(configDirectory, 'index.js')
 const configBundlePath = join(workDir, '.genoacms', 'genoa.config')
