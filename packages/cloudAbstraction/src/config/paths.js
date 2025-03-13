92e35eb3223cd60156f7e70f184fd78b9d42356d
Filip Holčík <filip.holcik.official@gmail.com>
2025-03-13 14:39:55 +0100

    feat(config): environment path resolution

--- packages/cloudAbstraction/src/config/paths.js
+++ packages/cloudAbstraction/src/config/paths.js
@@ -1,7 +1,8 @@
 import { join } from 'path'
 
 const workDir = process.cwd()
-const configDirectory = join(workDir, process.env.GENOA_CONFIG_PATH ?? 'genoa.config')
+const configEnvironment = process.env.DEV === 'true' ? '' : join('..', '..', '..')
+const configDirectory = join(workDir, configEnvironment, process.env.GENOA_CONFIG_PATH ?? 'genoa.config')
 const configPath = join(configDirectory, 'index.js')
 const configBundlePath = join(workDir, '.genoacms', 'genoa.config')
 
