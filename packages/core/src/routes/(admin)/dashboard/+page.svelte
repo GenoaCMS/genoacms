74b3ecf89c3b9de2d547789102aa12cf50179396
Filip Holčík <filip.holcik.official@gmail.com>
2025-03-15 15:43:41 +0100

    feat(dashboard): changed layout

--- packages/core/src/routes/(admin)/dashboard/+page.svelte
+++ packages/core/src/routes/(admin)/dashboard/+page.svelte
@@ -1,7 +1,20 @@
 <script lang="ts">
-  export let data
+    import { Button } from 'flowbite-svelte'
+
+  const { data } = $props()
 </script>
 
-dashboard
-
-hello {data.user.email}
+<div class="p-9">
+  <div class="w-full flex flex-wrap">
+    <div>
+      <h1 class="text-xl md:text-2xl m-2">
+        Welcome {data.user.email}
+      </h1>
+    </div>
+    <div class="ms-auto w-full md:w-auto">
+      <Button color="light" href="https://genoacms.github.io/docs" target="_blank" class="w-full">
+        Read the docs
+      </Button>
+    </div>
+  </div>
+</div>
