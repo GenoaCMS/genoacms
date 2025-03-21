29539b079f262c8a85d855f389932265631076fb
Filip Holčík <filip.holcik.official@gmail.com>
2025-03-21 16:56:27 +0100

    feat: create document, get collection

--- packages/adapter-postgres/src/index.js
+++ packages/adapter-postgres/src/index.js
@@ -1,18 +1,54 @@
-import { getProvider } from '@genoacms/cloudabstraction'
-import postgres from 'postgres'
+import { config, getProvider } from '@genoacms/cloudabstraction'
+import knex from 'knex'
+/**
+ * @import {Adapter} from '@genoacms/cloudabstraction/database'
+ */
 
 const ADAPTER_NAME = '@genoacms/adapter-postgres'
 const provider = getProvider('database', ADAPTER_NAME)
-const sql = postgres({
-  idle_timeout: 20,
-  ...provider.config
+const sql = knex({
+  client: 'pg',
+  connection: { user: provider.config.username, ...provider.config }
 })
-console.log(provider)
 
-async function testConnection() {
-  const test = await sql`SELECT * FROM Test`
-  console.log(test, test[0].name)
-  return
+async function testConnection () {
+  const test = await sql('Test').select('*')
+  console.log(test)
 }
 
-testConnection()
+/**
+  * @type {Adapter['createDocument']}
+  */
+const createDocument = async function (reference, document) {
+  await sql(reference.name).insert(document)
+  console.log('inserted')
+}
+
+/**
+  * @type {Adapter['getCollection']}
+  */
+const getCollection = async function (reference, queryParams = {}) {
+  const rows = await sql(reference.name).select('*')
+  console.log(rows)
+  return rows
+}
+
+// console.log(config.database)
+const collection = config.database.databases[0].collections[0]
+const testDocument = {
+  id: crypto.randomUUID(),
+  name: 'test',
+  description: 'desc',
+  sections: []
+}
+console.log(collection)
+
+// console.log(sql)
+// testConnection()
+// createDocument(collection, testDocument)
+getCollection(collection)
+
+export {
+  createDocument,
+  getCollection
+}
