6b4f9236d318836cb85af25be26d67831e67a471
Filip Holčík <filip.holcik.official@gmail.com>
2025-03-22 12:36:44 +0100

    feat: get object

--- packages/adapter-minio/src/index.js
+++ packages/adapter-minio/src/index.js
@@ -1,4 +1,4 @@
-import { getProvider } from '@genoacms/cloudabstraction'
+import { config, getProvider } from '@genoacms/cloudabstraction'
 import * as Minio from 'minio'
 
 const ADAPTER_PATH = '@genoacms/adapter-minio'
@@ -6,3 +6,31 @@ const ADAPTER_PATH = '@genoacms/adapter-minio'
 const provider = getProvider('storage', ADAPTER_PATH)
 const minioClient = new Minio.Client(provider.config)
 console.log(provider)
+
+const isBucketRegistered = (name) => {
+  const has = config.storage.buckets.find((bucket) => bucket.name === name &&
+    bucket.providerName === provider.name)
+  return !!has
+}
+const checkBucket = async (name) => {
+  const isRegistered = isBucketRegistered(name)
+  if (!isRegistered) {
+    throw new Error('bucket-unregistered')
+  }
+  const exists = await minioClient.bucketExists(name)
+  if (!exists) {
+    throw new Error('bucket-dont-exists')
+  }
+}
+
+const getObject = async ({ bucket, name }) => {
+  checkBucket()
+  const data = await minioClient.getObject(bucket, name)
+  return {
+    data
+  }
+}
+
+export {
+  getObject
+}
