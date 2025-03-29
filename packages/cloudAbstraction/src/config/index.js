e63e09673f7a3b925f684f419c9c0c5873d28678
Filip Holčík <filip.holcik.official@gmail.com>
2025-03-29 13:36:08 +0100

    feat: get deployment provider

--- packages/cloudAbstraction/src/config/index.js
+++ packages/cloudAbstraction/src/config/index.js
@@ -14,8 +14,7 @@ function getProvider (type, adapterPath) {
   /**
    * @type{undefined | import('./genoa.config.d.ts').Provider}
   */
-  const provider = config[type].providers.find((provider) => provider.adapterPath === adapterPath &&
-    provider.isInitialized !== true)
+  const provider = config[type].providers.find((provider) => provider.adapterPath === adapterPath && isUsed !== true)
   if (!provider) {
     throw new Error(`Unable to get the provider for ${type} with path ${adapterPath}`)
   }
@@ -23,5 +22,17 @@ function getProvider (type, adapterPath) {
   return provider
 }
 
-export { config, getProvider }
+/**
+ * @param {string} name
+ * @returns {import('../services/deployment/index.d.ts').DeploymentProvider}
+ */
+function getDeploymentProvider (name) {
+  const provider = config.deployment.providers.find(provider => provider.name === name)
+  if (!provider) {
+    throw new Error(`Unable to get deployment provider with name ${name}`)
+  }
+  return provider
+}
+
+export { config, getProvider, getDeploymentProvider }
 export { storageResource, reference } from './schemas.js'
