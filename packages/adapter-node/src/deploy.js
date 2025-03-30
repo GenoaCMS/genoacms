2b0fb2af0176b9a0c967045de4ce48dc1ba79229
Filip Holčík <filip.holcik.official@gmail.com>
2025-03-30 22:47:11 +0200

    feat: deployment procedure

--- packages/adapter-node/src/deploy.js
+++ packages/adapter-node/src/deploy.js
@@ -1,5 +1,15 @@
-async function deploy () {
+import { join } from 'node:path'
+import { cp } from 'node:fs/promises'
 
+
+async function deploy () {
+  const workDir = process.cwd()
+  const kitBuildDir = join(workDir, 'node_modules', '@genoacms', 'core', 'build')
+  const genoaBuildDir = join(workDir, 'build', 'cms')
+  await cp(kitBuildDir, genoaBuildDir, {
+    recursive: true,
+    force: true
+  })
 }
 
 export default deploy
