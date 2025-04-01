5dd39734f3aca927d4b86ba666b648e51cdf6fc5
Filip Holčík <filip.holcik.official@gmail.com>
2025-04-01 18:26:56 +0200

    fix: deploy script

--- packages/adapter-node/src/deploy.js
+++ packages/adapter-node/src/deploy.js
@@ -4,12 +4,20 @@ import { cp } from 'node:fs/promises'
 
 async function deploy () {
   const workDir = process.cwd()
-  const kitBuildDir = join(workDir, 'node_modules', '@genoacms', 'core', 'build')
-  const genoaBuildDir = join(workDir, 'build', 'cms')
-  await cp(kitBuildDir, genoaBuildDir, {
+  const buildDir = join(workDir, '.genoacms', 'build/')
+  const configBuildDir = join(workDir, '.genoacms', 'genoa.config')
+  const outputBuildDir = join(workDir, 'build/')
+  const outputConfigDir = join(workDir, 'build', 'genoa.config')
+  const buildMove = cp(buildDir, outputBuildDir, {
     recursive: true,
     force: true
   })
+  const configMove = cp(configBuildDir, outputConfigDir, {
+    recursive: true,
+    force: true
+  })
+  await Promise.all([buildMove, configMove])
 }
 
 export default deploy
+
